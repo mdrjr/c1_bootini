31e809fe9fed0589686bc53ccdf09db05aad939f
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-01-17 03:11:26 -0200

    ethernet improvement

--- c1_init.sh
+++ c1_init.sh
@@ -110,3 +110,7 @@ echo 32768 > /proc/sys/net/core/rps_sock_flow_entries
 echo 2048 > /sys/class/net/eth0/queues/rx-0/rps_flow_cnt
 echo f > /sys/class/net/eth0/queues/rx-0/rps_cpus
 echo c > /sys/class/net/eth0/queues/tx-0/xps_cpus
+
+# Move IRQ's of ethernet to CPU1/2
+echo 1,2 > /proc/irq/40/smp_affinity_list
+
