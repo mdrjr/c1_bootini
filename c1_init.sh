9fc27f22ce8fc8e353b22a5c622a41b964d95f3a
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2015-01-26 01:25:47 -0200

    1024x600 fix and tabings

--- c1_init.sh
+++ c1_init.sh
@@ -29,7 +29,7 @@ common_display_setup() {
         	800x480*) M="0 0 799 479" ;;
 	        vga*)  M="0 0 639 749" ;;
 	        800x600p60*) M="0 0 799 599" ;;
-	        1024x600p60h*) M="0 0 1023 566" ;;
+	        1024x600p60h*) M="0 0 1023 599" ;;
 	        1024x768p60h*) M="0 0 1023 767" ;;
 	        sxga*) M="0 0 1279 1023" ;;
         	1440x900p60*) M="0 0 1439 899" ;;
@@ -40,6 +40,9 @@ common_display_setup() {
         	1080*) M="0 0 1919 1079" ;;
         	1920x1200*) M="0 0 1919 1199" ;;
         	1680x1050p60*) M="0 0 1679 1049" ;;
+		1360x768p60*) M="0 0 1359 767" ;;
+		1366x768p60*) M="0 0 1365 767" ;;
+		1600x900p60*) M="0 0 1599 899" ;;
 	esac
 
 	echo $M > /sys/class/graphics/fb0/free_scale_axis
@@ -49,53 +52,23 @@ common_display_setup() {
 }
 
 case $mode in
-    800x480*) 		fbset -fb /dev/fb0 -g 800 480 800 960 $bpp;		common_display_setup ;;
-    vga*) 			fbset -fb /dev/fb0 -g 640 480 640 960 $bpp; 	common_display_setup ;;
-    480*)   		fbset -fb /dev/fb0 -g 720 480 720 960 $bpp;		common_display_setup ;;
-    800x600p60*)  	fbset -fb /dev/fb0 -g 800 600 800 1200 $bpp;	common_display_setup ;;
-    576*)   		fbset -fb /dev/fb0 -g 720 576 720 1152 $bpp;	common_display_setup ;;
-    1024x600p60h*) 	fbset -fb /dev/fb0 -g 1024 600 1024 1200 $bpp;	common_display_setup ;;
-    1024x768p60h*)	fbset -fb /dev/fb0 -g 1024 768 1024 1536 $bpp;	common_display_setup ;;
-    720*)   		fbset -fb /dev/fb0 -g 1280 720 1280 1440 $bpp;	common_display_setup ;;
-    800*)   		fbset -fb /dev/fb0 -g 1280 800 1280 1600 $bpp;	common_display_setup ;;
-    sxga*)  		fbset -fb /dev/fb0 -g 1280 1024 1280 2048 $bpp; common_display_setup ;;
-    1440x900p60*) 	fbset -fb /dev/fb0 -g 1440 900 1440 1800 $bpp;	common_display_setup ;;
-    1080*) 			fbset -fb /dev/fb0 -g 1920 1080 1920 2160 $bpp; common_display_setup ;;
-    1920x1200*) 	fbset -fb /dev/fb0 -g 1920 1200 1920 2400 $bpp; common_display_setup ;;
-    1360x768p60*)
-		fbset -fb /dev/fb0 -g 1920 1080 1920 3240 $bpp -rgba 8/24,8/16,8/8,0/0
-		fbset -fb /dev/fb1 -g 64 64 64 64 32 -rgba 8/24,8/16,8/8,8/0
-		echo $mode > /sys/class/display/mode
-		echo 0 > /sys/class/graphics/fb0/free_scale
-		echo 2 > /sys/class/graphics/fb0/freescale_mode
-		echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		echo 0 0 1359 767 > /sys/class/graphics/fb0/window_axis
-		echo 0x10001 > /sys/class/graphics/fb0/free_scale
-		echo 2 > /sys/module/rdma/parameters/enable
-		;;
-    1366x768p60*) 
-		fbset -fb /dev/fb0 -g 1920 1080 1920 3240 $bpp -rgba 8/24,8/16,8/8,0/0
-		fbset -fb /dev/fb1 -g 64 64 64 64 32 -rgba 8/24,8/16,8/8,8/0
-		echo $mode > /sys/class/display/mode
-		echo 0 > /sys/class/graphics/fb0/free_scale
-		echo 2 > /sys/class/graphics/fb0/freescale_mode
-		echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		echo 0 0 1365 767 > /sys/class/graphics/fb0/window_axis
-		echo 0x10001 > /sys/class/graphics/fb0/free_scale
-		echo 2 > /sys/module/rdma/parameters/enable
-		;;
-	1600x900p60*)
-		fbset -fb /dev/fb0 -g 1920 1080 1920 3240 $bpp -rgba 8/24,8/16,8/8,0/0
-		fbset -fb /dev/fb1 -g 64 64 64 64 32 -rgba 8/24,8/16,8/8,8/0
-		echo $mode > /sys/class/display/mode
-		echo 0 > /sys/class/graphics/fb0/free_scale
-		echo 2 > /sys/class/graphics/fb0/freescale_mode
-		echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		echo 0 0 1599 799 > /sys/class/graphics/fb0/window_axis
-		echo 0x10001 > /sys/class/graphics/fb0/free_scale
-		echo 2 > /sys/module/rdma/parameters/enable
-		;;
-	1680x1050p60*) fbset -fb /dev/fb0 -g 1680 1050 1680 2100 $bpp; common_display_setup ;;
+	800x480*)	fbset -fb /dev/fb0 -g 800 480 800 960 $bpp;	common_display_setup ;;
+	vga*)		fbset -fb /dev/fb0 -g 640 480 640 960 $bpp;	common_display_setup ;;
+	480*)		fbset -fb /dev/fb0 -g 720 480 720 960 $bpp;	common_display_setup ;;
+	800x600p60*)	fbset -fb /dev/fb0 -g 800 600 800 1200 $bpp;	common_display_setup ;;
+	576*)		fbset -fb /dev/fb0 -g 720 576 720 1152 $bpp;	common_display_setup ;;
+	1024x600p60h*)	fbset -fb /dev/fb0 -g 1024 600 1024 1200 $bpp;	common_display_setup ;;
+	1024x768p60h*)	fbset -fb /dev/fb0 -g 1024 768 1024 1536 $bpp;	common_display_setup ;;
+	720*)		fbset -fb /dev/fb0 -g 1280 720 1280 1440 $bpp;	common_display_setup ;;
+	800*)		fbset -fb /dev/fb0 -g 1280 800 1280 1600 $bpp;	common_display_setup ;;
+	sxga*)		fbset -fb /dev/fb0 -g 1280 1024 1280 2048 $bpp;	common_display_setup ;;
+	1440x900p60*)	fbset -fb /dev/fb0 -g 1440 900 1440 1800 $bpp;	common_display_setup ;;
+	1080*)		fbset -fb /dev/fb0 -g 1920 1080 1920 2160 $bpp;	common_display_setup ;;
+	1920x1200*)	fbset -fb /dev/fb0 -g 1920 1200 1920 2400 $bpp;	common_display_setup ;;
+	1360x768p60*)	fbset -fb /dev/fb0 -g 1360 768 1360 1536 $bpp;	common_display_setup ;;
+	1366x768p60*)	fbset -fb /dev/fb0 -g 1366 768 1366 1536 $bpp;	common_display_setup ;;
+	1600x900p60*)	fbset -fb /dev/fb0 -g 1600 900 1600 1800 $bpp;	common_display_setup ;;
+	1680x1050p60*)	fbset -fb /dev/fb0 -g 1680 1050 1680 2100 $bpp;	common_display_setup ;;
 	
 esac
 
@@ -113,4 +86,3 @@ echo c > /sys/class/net/eth0/queues/tx-0/xps_cpus
 
 # Move IRQ's of ethernet to CPU1/2
 echo 1,2 > /proc/irq/40/smp_affinity_list
-
